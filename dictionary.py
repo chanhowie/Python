21dce257298501a286728b0d368dbdca69149497
chanhowie <630340249@qq.com>
2022-01-12 15:36:02 +0800

    字符串的方法

--- dictionary.py
+++ dictionary.py
@@ -4,5 +4,16 @@ list=[{"name": "小明",
       {"name": "小六",
        "tel":"712587",
        "add": 336}]
-
-print(list)
+str1="你在干啥呢你在"
+print(str1.count("str1"))#显示次数
+print(str1.index("呢"))#显示位置
+""""
+str.capitalize  str.format      str.isupper     str.rindex      str.strip
+str.center      str.index       str.join        str.rjust       str.swapcase
+str.count       str.isalnum     str.ljust       str.rpartition  str.title
+str.decode      str.isalpha     str.lower       str.rsplit      str.translate
+str.encode      str.isdigit     str.lstrip      str.rstrip      str.upper
+str.endswith    str.islower     str.partition   str.split       str.zfill
+str.expandtabs  str.isspace     str.replace     str.splitlines  
+str.find        str.istitle     str.rfind       str.startswith  
+"""
