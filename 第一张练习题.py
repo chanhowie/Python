e9f8c0d71864129aaa785a6d19430a1309f9cf8d
chanhowie <630340249@qq.com>
2022-01-12 17:17:10 +0800

    简单做下题

---
+++ 第一张练习题.py
@@ -0,0 +1,15 @@
+num="0123456789"
+#2-5
+print(num[1:6])
+print(num[1:])
+print(num[:6])
+print(num[:])
+print(num[::2])
+print(num[1::2])
+print(num[2:-1])
+print(num[8:])
+print(num[-2:])
+print(num[-1::-1])
+print(num[-1:-11:-1])
+
+
