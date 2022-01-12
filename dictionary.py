54bc096f0658e926a5f24ab14ff237ae9d609027
chanhowie <630340249@qq.com>
2022-01-12 15:06:39 +0800

    dict

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,8 @@
-dic = {"name": "小明",
-       "age": 18,
-       "sex": 0,
-       "height":178,
-       "weight":120}
+list=[{"name": "小明",
+       "tel": "165432",
+       "add": 657},
+      {"name": "小六",
+       "tel":"712587",
+       "add": 336}]
 
-
-dic["age1"]=18
-dic["age1"]="xiaoxiao"
-dic.pop("age1")
-dic.pop("sex")#如果删除键值对不存在 就会报错
+print(list)
