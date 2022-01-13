82e909356be8794841dba1d3c45395204a79aaef
chanhowie <630340249@qq.com>
2022-01-13 16:11:24 +0800

    增加了删除功能，支持修改多个数据

--- 名片管理系统/tools.py
+++ 名片管理系统/tools.py
@@ -1,4 +1,10 @@
-def show ():
+namecard = []
+relist = ["1", "2", "3"]
+selelist = ["1", "2", "3", "0"]  # 使用列表方法限制输入类型，可以排除浮点数或其他
+seredlist = []
+
+
+def show():
     strlist = ["", "欢迎使用名片管理系统",
                "请选择功能",
                "①：增加名片",
@@ -10,23 +16,67 @@ def show ():
     for strout in strlist[1:]:
         print("┃%s┃" % strout.center(13, "　"))
     print(strlist[0].center(24, "━"))
-namecard = []
-def newcard():
+
+
+def newcarda():
     newname = input("输入姓名")
     newage = input("输入年龄")
     newtel = input("输入电话")
-    namecard.append({"name": newname, "age": newage, "tel": newtel})
-    print("添加成功")
+    if({"name": newname, "age": newage, "tel": newtel} in namecard):
+        print("列表中存在相同数据，请重新输入")
+    else:
+        namecard.append({"name": newname, "age": newage, "tel": newtel})
+        print("添加成功")
+
+
 def sercard():
     sername = input("输入要查找的姓名")
+    first_flag = True  # 用来标识是否为第一次出现
     for showcard in namecard:
         if showcard["name"] == sername:
-            print("姓名\t年龄\t电话")
-            print("%s\t%s\t%s" % (showcard["name"], showcard["age"], showcard["tel"]))
-            break
-    else:
-        print("没有找到此人")
+            seredlist.append(namecard.index(showcard))
+            if first_flag == True:
+                print("姓名\t\t\t年龄\t\t\t电话")
+                first_flag = False;
+            print("%s\t\t\t%s\t\t\t%s" % (showcard["name"], showcard["age"], showcard["tel"]))
+    
+    if first_flag == True:
+        print("没有找到%s" % sername)
+        return
+    
+    deal(seredlist)
+    seredlist.clear()#退出后清除搜索结果
+
+
 def showcard():
-    print("姓名\t年龄\t电话")
+    if len(namecard) == 0:
+        print("列表为空，请先添加名片")
+        return
+    print("姓名\t\t\t年龄\t\t\t电话")
+    print("-" * 28)
     for showcard in namecard:
-        print("%s\t%s\t%s" % (showcard["name"], showcard["age"], showcard["tel"]))
+        print("%s\t\t\t%s\t\t\t%s" % (showcard["name"], showcard["age"], showcard["tel"]))
+
+
+def deal(seredlist):
+    resec = input("①  删除  ②  修改  ③  返回")
+    if resec in relist:
+        if resec == "1":
+            for i in reversed(seredlist): #倒序删除，防止删错位置
+                del namecard[i]
+        if resec =="2":
+            modify=True
+            for i in reversed(seredlist):
+                modify = True
+                while(modify):
+                    newname = input("输入姓名")
+                    newage = input("输入年龄")
+                    newtel = input("输入电话")
+                    if ({"name": newname, "age": newage, "tel": newtel} in namecard):
+                        print("列表中存在相同数据，请重新输入")
+                    else:
+                        namecard[i]={"name":newname,"age":newage,"tel":newtel}
+                        print("修改成功")
+                        modify=False
+        if resec == "3":
+            return
--- 名片管理系统/名片管理系统.py
+++ 名片管理系统/名片管理系统.py
@@ -3,7 +3,7 @@ import tools
 
 def sec_func(selection):
     if selection == "1":
-        tools.newcard()
+        tools.newcarda()
     elif selection == "2":
         tools.sercard()
     elif selection == "3":
@@ -13,11 +13,9 @@ def sec_func(selection):
 while True:
     # TODO 显示菜单
     tools.show()
-    
-    selelist = ["1", "2", "3", "0"]  # 使用列表方法限制输入类型，可以排除浮点数或其他
     select = input()
     print("你输入了【%s】" % select)
-    if select in selelist:
+    if select in tools.selelist:
         if select == "0":
             print("欢迎下次使用")
             break
